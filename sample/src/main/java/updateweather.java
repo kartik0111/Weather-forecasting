e4a3503f5b67dddf657d564e6bc0f2da642abd3d
Kartik Bunkar <kartikbunkar070@gmail.com>
2022-02-07 11:03:25 +0530

    uuu

--- sample/src/main/java/updateweather.java
+++ sample/src/main/java/updateweather.java
@@ -1,14 +1,37 @@
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.mysqlclient.MySQLConnectOptions;
 import io.vertx.mysqlclient.MySQLPool;
 import io.vertx.sqlclient.PoolOptions;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.Tuple;
-
 public class updateweather {
+    public static void func() {
+        Vertx vertx = Vertx.vertx();
+
+        String url = "api.openweathermap.org";
+        WebClient client = WebClient.create(vertx, new WebClientOptions().setDefaultPort(80).setDefaultHost(url));
+        client.get("/data/2.5/weather?q=lucknow,in&units=metric&appid=994be899da8cf911d5ab3436fa020441").as(BodyCodec.string()).send(ar -> {
+            if (ar.succeeded()) {
+                HttpResponse<String> response = ar.result();
+                System.out.println("Got HTTP response body");
+                System.out.println(response.body());
+                nono(response);
+                //System.out.println(str);
+            } else {
+                ar.cause().printStackTrace();
+            }
+            client.close();
+        });
+
+
+        //
+    }
     public static void nono(HttpResponse<String> res) {
         Vertx vertx = Vertx.vertx();
         JsonObject jj = new JsonObject(res.body());
@@ -18,43 +41,26 @@ public class updateweather {
                 .setDatabase("sql6469448")
                 .setUser("sql6469448")
                 .setPassword("tprZlW1uah");
-
-        // Pool options
         PoolOptions poolOptions = new PoolOptions()
                 .setMaxSize(5);
-
-        // Create the client pool
         MySQLPool client = MySQLPool.pool(vertx, connectOptions, poolOptions);
-
-// A simple query
-  /*      client.query("SELECT * FROM Userss").execute(ar -> {
-            if (ar.succeeded()) {
-                RowSet<Row> rows = ar.result();
-                //System.out.println("Got " + result.size() + " rows ");
-                for (Row row : rows) {
-                    System.out.println("User " + row.getString(0));
-                }
-            } else {
-                System.out.println("Failure: " + ar.cause().getMessage());
-            }
-*/
         String city = jj.getString("name");
         String weath = jj.getString("main");
         System.out.println(city);
         System.out.println(weath);
-        client.preparedQuery("INSERT INTO Saveweather (city , weather) VALUES (?,?)")
-                .execute(Tuple.of(city,weath), ar -> {
+        client.preparedQuery("UPDATE Saveweather SET weather = ? WHERE city = ?")
+                .execute(Tuple.of(weath,city), ar -> {
                     if (ar.succeeded()) {
                         RowSet<Row> rows = ar.result();
                         System.out.println("success");
                     } else {
                         System.out.println("Failure: " + ar.cause().getMessage());
                     }
-
                     // Now close the pool
-
                     client.close();
                 });
     }
-
+    public static void main(String[] args) {
+        func();
+    }
 }
